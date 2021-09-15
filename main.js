4ef48f20b957454ac33841cca241e2301b375ab3
Sebastian Saporiti <seba.saporiti@gmail.com>
2021-09-14 23:05:11 -0300

    update

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-icon').addEventListener('click', () =>{
+    document.querySelector('.flex-cont1').classList.toggle('flex-cont2');
+    document.querySelector('.fa').classList.toggle('fa-bars')
+    document.querySelector('.fa').classList.toggle('fa-times')
+});
---
+++ main_barilago.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-icon').addEventListener('click', () =>{
+    document.querySelector('.menu').classList.toggle('menu2');
+    document.querySelector('.fa').classList.toggle('fa-bars')
+    document.querySelector('.fa').classList.toggle('fa-times')
+});
